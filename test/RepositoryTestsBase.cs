e84ca59e51f195edb4239bb397b5b0b3180a9653
bigabdoul <kababdoul@hotmail.com>
2018-07-02 08:47:46 +0000

    add 2 methods to test base class

--- test/RepositoryTestsBase.cs
+++ test/RepositoryTestsBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using CoreRepository.Test.Data;
@@ -56,16 +57,20 @@ namespace CoreRepository.Test
         protected async Task InsertCategoriesAsync()
         {
             var categories = DIProvider.GetSqliteRepository<Category>();
+            AddCategories(categories);
+            await categories.SaveChangesAsync();
+        }
+
+        protected void AddCategories(IRepository<Category> categories)
+        {
             var cars = new Category { Id = 1, Name = "Cars" };
             var computers = new Category { Id = 2, Name = "Computers" };
 
             categories.Add(cars);
             categories.Add(computers);
-
-            await categories.SaveChangesAsync();
         }
 
-        protected async Task InsertProductsAsync()
+        protected virtual async Task InsertProductsAsync()
         {
             var categories = DIProvider.GetSqliteRepository<Category>();
             var products = DIProvider.GetSqliteRepository<Product>();
@@ -73,31 +78,8 @@ namespace CoreRepository.Test
             var cars = await categories.GetAsync(q => q.Where(c => c.Name == "Cars").Single());
             var computers = await categories.GetAsync(q => q.Where(c => c.Name == "Computers").Single());
 
-            products.Add(new Product { Id = 1, Name = "Audi", Category = cars });
-            products.Add(new Product { Id = 2, Name = "Porsche", Category = cars });
-            products.Add(new Product { Id = 3, Name = "Volkswagen", Category = cars });
-            products.Add(new Product { Id = 4, Name = "BMW", Category = cars });
-            products.Add(new Product { Id = 5, Name = "Mercedes Benz", Category = cars });
-            products.Add(new Product { Id = 6, Name = "Peugeot", Category = cars });
-            products.Add(new Product { Id = 7, Name = "Citroën", Category = cars });
-            products.Add(new Product { Id = 8, Name = "Toyota", Category = cars });
-            products.Add(new Product { Id = 9, Name = "Suzuki", Category = cars });
-            products.Add(new Product { Id = 10, Name = "Lamborghini", Category = cars });
-            products.Add(new Product { Id = 11, Name = "Ferrari", Category = cars });
-            products.Add(new Product { Id = 12, Name = "GMC", Category = cars });
-
-            products.Add(new Product { Id = 13, Name = "HP", Category = computers });
-            products.Add(new Product { Id = 14, Name = "Dell", Category = computers });
-            products.Add(new Product { Id = 15, Name = "Toshiba", Category = computers });
-            products.Add(new Product { Id = 16, Name = "Compaq", Category = computers });
-            products.Add(new Product { Id = 17, Name = "MacBook Pro", Category = computers });
-            products.Add(new Product { Id = 18, Name = "Google Chromebook", Category = computers });
-            products.Add(new Product { Id = 19, Name = "Sony", Category = computers });
-            products.Add(new Product { Id = 20, Name = "Asus", Category = computers });
-            products.Add(new Product { Id = 21, Name = "Acer", Category = computers });
-            products.Add(new Product { Id = 22, Name = "Surface Pro", Category = computers });
-            products.Add(new Product { Id = 23, Name = "Samsung", Category = computers });
-            products.Add(new Product { Id = 24, Name = "Lenovo", Category = computers });
+            AddCars(products, cars);
+            AddComputers(products, computers);
 
             await products.SaveChangesAsync();
         }
@@ -137,6 +119,38 @@ namespace CoreRepository.Test
                 }
             }
             return _testDbContextOptions;
+        }
+
+        protected void AddComputers(Repository<Product> products, Category computers)
+        {
+            products.Add(new Product { Id = 13, Name = "HP", Category = computers });
+            products.Add(new Product { Id = 14, Name = "Dell", Category = computers });
+            products.Add(new Product { Id = 15, Name = "Toshiba", Category = computers });
+            products.Add(new Product { Id = 16, Name = "Compaq", Category = computers });
+            products.Add(new Product { Id = 17, Name = "MacBook Pro", Category = computers });
+            products.Add(new Product { Id = 18, Name = "Google Chromebook", Category = computers });
+            products.Add(new Product { Id = 19, Name = "Sony", Category = computers });
+            products.Add(new Product { Id = 20, Name = "Asus", Category = computers });
+            products.Add(new Product { Id = 21, Name = "Acer", Category = computers });
+            products.Add(new Product { Id = 22, Name = "Surface Pro", Category = computers });
+            products.Add(new Product { Id = 23, Name = "Samsung", Category = computers });
+            products.Add(new Product { Id = 24, Name = "Lenovo", Category = computers });
+        }
+
+        protected void AddCars(Repository<Product> products, Category cars)
+        {
+            products.Add(new Product { Id = 1, Name = "Audi", Category = cars });
+            products.Add(new Product { Id = 2, Name = "Porsche", Category = cars });
+            products.Add(new Product { Id = 3, Name = "Volkswagen", Category = cars });
+            products.Add(new Product { Id = 4, Name = "BMW", Category = cars });
+            products.Add(new Product { Id = 5, Name = "Mercedes Benz", Category = cars });
+            products.Add(new Product { Id = 6, Name = "Peugeot", Category = cars });
+            products.Add(new Product { Id = 7, Name = "Citroën", Category = cars });
+            products.Add(new Product { Id = 8, Name = "Toyota", Category = cars });
+            products.Add(new Product { Id = 9, Name = "Suzuki", Category = cars });
+            products.Add(new Product { Id = 10, Name = "Lamborghini", Category = cars });
+            products.Add(new Product { Id = 11, Name = "Ferrari", Category = cars });
+            products.Add(new Product { Id = 12, Name = "GMC", Category = cars });
         }
 
         #endregion
